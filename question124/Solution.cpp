57c51a0456a2d4ac6966b0e735bc27395d119ca0
leoli <836864734@qq.com>
2021-04-08 09:55:35 +0800

    Answer to Question 124

--- question124/Solution.cpp
+++ question124/Solution.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include "math.h"
 #include "../bean/Node.cpp"
+
 class Solution {
+private:
+    int maxSum = INT32_MIN;
 public:
-    int maxPathSum(TreeNode* root) {
-        // 遍历，求每个节点的左右子树的最大和
+    int maxPathSum(TreeNode *root) {
+        traverse(root);
+        return maxSum;
+    }
+
+    int traverse(TreeNode *node) {
+        // 叶节点
+        if (node == nullptr) {
+            return 0;
+        }
+        // 求子节点的最大贡献值
+        int maxLeft = fmax(traverse(node->left), 0);
+        int maxRight = fmax(traverse(node->right), 0);
+
+        // 更新答案
+        maxSum = fmax(maxSum, node->val + maxLeft + maxRight);
+
+        // 求本节点的最大贡献值
+        return fmax(maxLeft, maxRight) + node->val;
     }
 };
